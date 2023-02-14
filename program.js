5dd725415d1f7f877e5520a2a711452d2189e5a8
i-ght <toph.ght@gmail.com>
2023-02-14 15:41:56 -0700

    upgrades

--- program.js
+++ program.js
@@ -2,11 +2,8 @@ const XAndOrY =
   Object.freeze({
     NONE:  0,
     X:     1,
-    Y:     2,
-    XANDY: 4
+    Y:     2
   });
-
-let bouncyBall;
 
 class Force {
   constructor(domain, momentum) {
@@ -25,43 +22,62 @@ class Force {
     if (this.domain.x > width || this.domain.x < 0) {
       x = true;
     }
+    
     if (this.domain.y > height || this.domain.y < 0) {
       y = true;
     }
     
     if (x && y)  {
-      return XAndOrY.XANDY;
+      return XAndOrY.X | XAndOrY.Y;
     }
-    if (x && !y) {
+    else if (x && !y) {
       return XAndOrY.X;
     }
-    if (y && !x) {
+    else if (y && !x) {
       return XAndOrY.Y;
-    }
-    
+    } /*else {*/
+      
     return XAndOrY.NONE;
+    
+    /*}*/
   }
+}
+
+class BouncyBall extends Force {
+  
+}
+
+const X = 0;
+const Y = 1;
+
+let bouncyBall;
+
+function constructBouncyBall() {
+  
+  const domainXY = [1.1618, 1];
+  const domain = createVector(domainXY[X], domainXY[Y]);
+  
+  const momentumXY = [1.618 * 3, 3];
+  const momentum = createVector(momentumXY[X], momentumXY[Y]);
+  
+  return new BouncyBall(
+      domain,
+      momentum
+  );
 }
 
 function setup() {
   createCanvas(333, 333);
   stroke(255, 0, 255);
-  bouncyBall =
-    new Force(
-      createVector(1.618, 1),
-      createVector(1.618 * 3, 1 * 3)
-    );
+  bouncyBall = constructBouncyBall();
 }
 
 function draw() {
   background(255);
-  circle(bouncyBall.domain.x, bouncyBall.domain.y, 34);
-  
-  bouncyBall.advance();
   
   let bounds = bouncyBall.outOfBounds();
 
-  if (bounds & XAndOrY.XANDY) {
+  if (bounds & XAndOrY.X && bounds & XAndOrY.Y) {
       console.log("XY");
       bouncyBall.momentum.mult(-1, -1);  
   } else if (bounds & XAndOrY.Y) {
@@ -69,4 +85,8 @@ function draw() {
   } else if (bounds & XAndOrY.X) {
       bouncyBall.momentum.mult(-1, 1);
   }
+  
+  bouncyBall.advance();
+  circle(bouncyBall.domain.x, bouncyBall.domain.y, 99);
+  
 }
